05ff4ba5a1812528d185de732bfd9c8bc8eb7dc2
Lucas Molas <schomatis@gmail.com>
2020-04-06 14:12:41 -0700

    [FIXUP] add Frame to HierarchicalError

--- validation/hierarchical_error.go
+++ validation/hierarchical_error.go
@@ -1,5 +1,11 @@
 package validation
 
+import (
+	"fmt"
+
+	"golang.org/x/xerrors"
+)
+
 // This is incorrect. Its purpose is to illustrate my intent to see if there's a
 // clean and simple way to do this (and even if it's worth doing it). `Errorf`,
 //  * https://pkg.go.dev/golang.org/x/xerrors?tab=doc#Errorf
@@ -15,6 +21,7 @@ package validation
 type HierarchicalError struct {
 	parent error
 	msg    string
+	frame  xerrors.Frame
 }
 
 func (e *HierarchicalError) Error() string {
@@ -24,20 +31,43 @@ func (e *HierarchicalError) Error() string {
 	//  I'm using references in the opposite direction.
 }
 
+func (e *HierarchicalError) Format(s fmt.State, v rune) { xerrors.FormatError(e, s, v) }
+
+func (e *HierarchicalError) FormatError(p xerrors.Printer) (next error) {
+	p.Print(e.msg)
+	e.frame.Format(p)
+	return e.parent
+	// FIXME: Again, going in the wrong direction.
+}
+
 func (e *HierarchicalError) Unwrap() error {
 	return e.parent
 }
 
-func ErrorWrapString(parent error, msg string) error {
-	return &HierarchicalError{
-		parent: parent,
-		msg:    msg,
-	}
+// FIXME: Need to decouple the error itself from its type. Once that is done
+//  we can create duplicate errors with different frame info where the unique
+//  part is the type the error points to within its hierarchy.
+// FIXME: See where we should use this, or if anywhere a `HierarchicalError` is
+//  returned (in which case there should be a more automatic way to do it than
+//  adding this call in every returned error).
+func (e *HierarchicalError) WithFrameInfo() error {
+	return &HierarchicalError{parent: e.parent, msg: e.msg, frame: xerrors.Caller(1)}
 }
 
-func ErrorWrapError(parent error, err error) error {
-	return &HierarchicalError{
-		parent: parent,
-		msg:    err.Error(),
-	}
+func GetCallerFrame() xerrors.Frame {
+	frame := xerrors.Frame{}
+	// if internal.EnableTrace {
+	frame = xerrors.Caller(1)
+	// }
+	// FIXME: Can't reference internal package.
+
+	return frame
+}
+
+func ErrorWrapString(parent error, msg string) *HierarchicalError {
+	return &HierarchicalError{parent: parent, msg: msg, frame: xerrors.Frame{}}
+}
+
+func ErrorWrapError(parent error, err error) *HierarchicalError {
+	return &HierarchicalError{parent: parent, msg: err.Error(), frame: xerrors.Frame{}}
 }
